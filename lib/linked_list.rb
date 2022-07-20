4af8b0319da1a055c66cc0284ef0e86c5ef4bec9
Sarah Dylan <SarahmDylan@gmail.com>
2022-07-20 09:11:09 -0700

    Add methods: append, prepend, size, head, tail

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,77 @@
 class LinkedList
+  require_relative './node'
+
   def initialize
     @first_node = nil
     @last_node = nil
   end
+
+  def append(value)
+    if @first_node.nil?
+      @first_node = Node.new(value)
+      @last_node = @first_node
+    else
+      @last_node.next_node = Node.new(value)
+      @last_node = @last_node.next_node
+    end
+  end
+
+  def prepend(value)
+    if @first_node.nil?
+      @first_node = Node.new(value)
+      @last_node = Node.new(value)
+    else
+      temp = Node.new(value)
+      temp.next_node = @first_node
+      @first_node = temp
+    end
+  end
+
+  def size
+    return 0 unless @first_node
+
+    current_node = @first_node
+    counter = 1
+    while current_node.next_node
+      counter += 1
+      current_node = current_node.next_node
+    end
+    counter
+  end
+
+  def head
+    @first_node.value
+  end
+
+  def tail
+    @last_node.value
+  end
+
+  def at(index)
+    return nil unless @first_node
+
+    current_node = @first_node
+    counter = 0
+
+    until counter == index
+      return nil unless current_node.next_node
+
+      current_node = current_node.next_node
+      counter += 1
+    end
+    current_node.value
+  end
+
+  def pop() end 
 end
+
+list = LinkedList.new
+list.append(2)
+list.prepend(1)
+list.append(3)
+list.append(4)
+p list.size
+p list
+p list.head
+p list.tail
+p list.at(2)
